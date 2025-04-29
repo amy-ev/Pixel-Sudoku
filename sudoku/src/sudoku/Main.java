df52a248c241e652fc42e1effe66321373ddc355
amy-ev <amyevemy@hotmail.co.uk>
2025-04-29 16:53:21 +0100

    JFrame main class

--- sudoku/src/sudoku/Main.java
+++ sudoku/src/sudoku/Main.java
@@ -6,15 +6,15 @@ import java.awt.*;
 public class Main extends JFrame {
 
     public Main() {
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         SudokuGrid grid = new SudokuGrid();
         grid.newGrid();
-        frame.add(grid);
-        frame.pack();
-        frame.setVisible(true);
-
+        super.add(grid);
+        super.pack();
+        super.setVisible(true);
     }
+
     public static void main(String[] args) {
         new Main();
     }
